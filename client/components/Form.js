a4f6dec1f054cedc7d1f99785ac897e2bd422f15
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-03-19 15:28:47 -0400

    Feature: Created new Model Entry component.'

--- client/components/Form.js
+++ client/components/Form.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import FileSaver from 'file-saver'
+import FileSaver from 'file-saver';
+import ModelEntry from './ModelEntry';
 
-export default class YMLForm extends Component {
+export default class Form extends Component {
   constructor () {
     super();
     this.state = {
@@ -40,18 +41,8 @@ export default class YMLForm extends Component {
             <input type="radio" id="rails" name="file_structure" value="rails" required />
             <label forhtml="rails">Rails</label>
           </div>
-            <label forhtml="model">Model</label>
-            <input type="text" name="model" required />
-            <label forhtml="property">Properties</label>
-            <input type="text" name="property_name" required />
-            <select name="property" required>
-              <option value="string">String</option>
-              <option value="boolean">Boolean</option>
-              <option value="number">Number</option>
-              <option value="array">Array</option>
-              <option value="object">Object</option>
-            </select>
         </form>
+        <ModelEntry handleChange={this.handleChange} />
         <button type="button" onClick={() => this.handleSubmit(event)}>SUBMIT</button>
       </div>
     );
