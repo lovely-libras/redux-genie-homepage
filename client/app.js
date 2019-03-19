a3720d759b33e65fb2aaa8fb574ed64dc2aec34d
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-03-19 12:57:28 -0400

    Feature: Added Navbar.

---
+++ client/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div className="yml_form_container">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav">
+        <h1>THIS IS THE NAV BAR</h1>
+      </div>
+    );
+  }
+}
