77e38c1bd918031a401437dc0db546d483b739fe
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-03-19 14:25:18 -0400

    Feature: Moved forms to own module.' ' ' q '

--- client/app.js
+++ client/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import YMLForm from './components/YMLForm';
+import Form from './components/Form';
 
 const App = () => {
   return (
     <div className="yml_form_container">
       <Navbar />
-      <YMLForm />
+      <Form />
     </div>
   );
 };
