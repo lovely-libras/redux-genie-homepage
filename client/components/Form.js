b973d515e1c2ee92f3806efcd72640856da9f7b1
Nick <nickpereslugoff@gmail.com>
2019-03-20 14:29:03 -0400

    commiting before refactoring to more simple form strucutre

--- client/components/Form.js
+++ client/components/Form.js
@@ -7,21 +7,57 @@ export default class Form extends Component {
     super();
     this.state = {
       file_structure: '',
-      model: '',
-      property_name: '',
-      property: ''
+      models: [],
+      numForms: 1,
+      forms: {
+        ME0: { // === event.target.dataSet.modelName
+          name: '',
+          properties: [
+            {name: 'propname', type: 'string'},
+          ]
+        }
+      }
     };
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.addModelEntry = this.addModelEntry.bind(this)
+    this.handleParentChange = this.handleParentChange.bind(this)
+    this.setModelName = this.setModelName.bind(this)
   }
-  handleChange (event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
+
+  // handleChange (event) {
+  //   this.setState({
+  //     [event.target.name]: event.target.value
+  //   });
+  // }
+
+  setModelName (event){
+    const {modelName} = event.target.dataset
+    const {forms} = this.state
+    const modelObj = this.state.forms[modelName]
+    const {properties} = this.state.modelName.forms
+    this.setState({ forms: {...forms, [modelName]: {...modelObj, name: event.target.value}, properties: [...properties]} }, () => console.log(this.state))
   }
+
+  setPropertName(event){
+    // const {modelName} = event.target.dataset
+    // const {forms} = this.state
+    // const modelObj = this.state.forms[modelName]
+    // let {properties} = this.state.modelName.forms
+    // this.setState({ forms: {...forms, [modelName]: {...modelObj, properties: }}})
+  }
+
+  handleParentChange(event){
+    if(event.target.name === "model"){
+      setModelName(event)
+    } else if(event.target.name === "property_name"){
+      console.log("Property input",event.target)
+    } else if(event.target.name === "property"){
+      console.log("Property select",event.target)
+    }
+  }
+
   handleSubmit (event) {
     event.preventDefault();
-    console.log(this.state);
     let file = new File([
       [`Structure: ${this.state.file_structure}\n`],
       ['\n'],
@@ -31,19 +67,34 @@ export default class Form extends Component {
     ], "lamp.config.yml", {type: "text/plain;charset=utf-8"});
     FileSaver.saveAs(file);
   }
+
+  addModelEntry(){
+    this.setState({ numForms: this.state.numForms + 1, forms: {...this.state.forms, [`ME${this.state.numForms}`]: {
+      name: ''
+    }}})
+  }
+
+
   render() {
+    let modelForms = []
+
+    for(let i = 0; i < this.state.numForms; i++){
+        modelForms.push(<ModelEntry data={`ME${i}`} handleData={this.handleData} key={`ME${i}`}/>)
+    }
+
     return (
-      <div>
-        <form onChange={(event) => this.handleChange(event)} className="yml_form">
-          <div>
+      <div className="yml_form" onChange={(event) => this.handleParentChange(event)}>
+        <form >
+          <div className="structure-entry">
             <input type="radio" id="ducks" name="file_structure" value="ducks" required />
             <label forhtml="ducks">Ducks</label>
             <input type="radio" id="rails" name="file_structure" value="rails" required />
             <label forhtml="rails">Rails</label>
           </div>
+          {modelForms}
         </form>
-        <ModelEntry handleChange={this.handleChange} />
-        <button type="button" onClick={() => this.handleSubmit(event)}>SUBMIT</button>
+        <button onClick={this.addModelEntry}>New Model</button>
+        <button type="button" onClick={() => console.log(this.state)}>SUBMIT</button>
       </div>
     );
   }
--- client/components/ModelEntry.js
+++ client/components/ModelEntry.js
@@ -1,24 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PropertyEntry from "./PropertyEntry";
 
 export default class ModelEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      model: "",
+      properties: [],
+      numProperties: 1
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+    this.createObject = this.createObject.bind(this);
+    this.addPropertyEntry = this.addPropertyEntry.bind(this);
+  }
+
+  // handleChange(event) {
+  //   this.setState({ [event.target.name]: event.target.value });
+  // }
+
+  createObject() {
+    let obj = {};
+    obj[this.state.model] = [];
+
+    let innerObj = {};
+    innerObj[this.state.property_name] = this.state.property;
+    obj[this.state.model].push(innerObj);
+
+    this.props.handleData(obj);
+  }
+
+  addPropertyEntry() {
+    this.setState({ numProperties: this.state.numProperties + 1 });
+  }
+
   render() {
+    let propertyEntries = [];
+
+    for (let i = 0; i < this.state.numProperties; i++) {
+      propertyEntries.push(<PropertyEntry  key={`PE${i}`} model-name={this.props.data}/>);
+    }
     return (
-      <div>
-        <form onChange={(event) => this.props.handleChange(event)} className="yml_form">
+      <div className="model-entry">
+        <div className="model-name-entry">
           <label forhtml="model">Model</label>
-          <input type="text" name="model" required />
-          <label forhtml="property">Properties</label>
-          <input type="text" name="property_name" required />
-          <select name="property" required>
-            <option value="string">String</option>
-            <option value="boolean">Boolean</option>
-            <option value="number">Number</option>
-            <option value="array">Array</option>
-            <option value="object">Object</option>
-          </select>
-        </form>
+          <input data-model-name={this.props.data} type="text" name="model" required />
+        </div>
+        <label forhtml="property">Properties</label>
+        {propertyEntries}
+        <div className="property-buttons">
+          <button onClick={() => this.addPropertyEntry()}>New property</button>
+          <button className="modelButton" onClick={() => this.createObject()}>
+            Add model to file
+          </button>
+        </div>
       </div>
     );
   }
 }
-
