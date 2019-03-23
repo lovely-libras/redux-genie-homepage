2e9098361854334f1b8c4d6f11375e4dffb0109d
Nick <nickpereslugoff@gmail.com>
2019-03-23 18:40:22 -0400

    added public folder

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
+app.use(express.static(path.join(__dirname, '../public/')))
+
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
