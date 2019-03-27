2dbd7ff69156a9d4fe139ecdd605639d3213a5f0
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-03-27 17:17:59 -0400

    Fix: Documenation for site complete.

--- client/components/DocumentationText.js
+++ client/components/DocumentationText.js
@@ -9,38 +9,46 @@ import 'brace/theme/solarized_light';
 const DocumentationText = () => {
   const text1 = `Structure: Ducks # Two options: Rails || Ducks
 
-        Models:
-          - Dux:
+Models:
+  - Ducks:
 
-            Slice:
-              - Name: string
-              - Quacking: Boolean
-              - Ducklings: Object
-              - Fly2Gether: Boolean
+    Slice:
+      - Name: String
+      - Quacking: Boolean
+      - Ducklings: Object
+      - Fly2Gether: Boolean
 
-            CRUD: false
+    CRUD: false
 
-            Actions:
-              - countDux
-              - migrateDux
-              - quackOne
+    Actions:
+      - countDucks
+      - migrateDucks
 
-            Thunks:
-              - getAll: "/api/Dux"
-              - getOne: "/api/Dux/:dux"
-              - Terminator:
+    Thunks:
+      - getAll:
+        - "/api/Ducks"
+        - countDucks
+      - getOne:
+        - "/api/Ducks/:ducks"
+        - migrateDucks
 
-            Slice:
-              - WillBeBack: Boolean
-              - OneLiners: Array
-              - Sequels: Number
+  - CobraChicken:
 
-            Actions:
-              - killJohnConnor
-              - backInTime
-            Thunks:
-              - getAll: "api/terminator"
-              - getOne: "api/terminator/:terminator"`;
+    Slice:
+      - Rage: Boolean
+      - Goslings: Array
+      - Feathers: String
+
+    Actions:
+      - attackTarget
+      - defecateEverywhere
+
+    Thunks:
+      - getAll:
+        - "api/geese"
+        - countGeese
+      - getOne: "api/geese/:gooseId"
+`;
 
   const text2 = `
 // lamp.config.yml
@@ -48,15 +56,14 @@ const DocumentationText = () => {
 ...
 
 Models:
-
-  - Dux:
+  - Ducks:
 
     Slice:
-
-      - Name: string
+      - Name: String
       - Quacking: Boolean
       - Ducklings: Object
       - Fly2Gether: Boolean
+
     CRUD: false
 
 ...
@@ -106,6 +113,7 @@ Models:
 
   return (
     <div id="documentation-container" className="form-style">
+
       <h1 id="reduxgenie">Redux Genie</h1>
 
       <h3 id="cheatcodesforredux">Summon the Genie!</h3>
@@ -114,8 +122,10 @@ Models:
         Redux Genie is a development tool that writes the boilerplate code for your Redux components. You can create a store from scratch, and then inject new files and their corresponding code into your project through the command line interface. Now you can spend less time managing your Redux store without abstracting its functionality, while avoiding some common errors.
       </p>
 
+      <h2 id="store-declaration">Using this site</h2>
+
       <p>
-        Further, by utilizing our companion website, you can easily generate a YML file that contains the parameters you need to construct your Redux store. Place the YML file within your project's root directory, and then summon the genie to automatically create your front-end boilerplate.
+        By utilizing our companion website, you can easily generate a YML configuration file that contains the parameters you need to construct your Redux store. Place the YML file within your project's root directory, and then summon the genie to automatically create your front-end boilerplate.
       </p>
 
       <p>
@@ -142,8 +152,12 @@ Models:
         With Redux Genie now installed, all you need to get started is a YML file.
       </p>
 
+      <h2 id="store-declaration">
+        What's a YML file?
+      </h2>
+
       <p>
-        "What's a YML file?", you may ask. It's a human-readable text file that allows you to define the parameters of your project's state. Redux Genie seeks out the lamp.config.yml file in your root directory, and like magic constructs the store according to the parameters it contains!
+        A YML file (*.yml) is a human-readable text file that allows you to define the parameters of your project's state. Redux Genie seeks out the <b>lamp.config.yml</b> file in your root directory, and like magic constructs the store according to the parameters it contains!
       </p>
 
       <div id="d-editor-one">
@@ -161,11 +175,11 @@ Models:
       </div>
 
       <blockquote>
-        <p>A sample lamp.config.yml file.</p>
+        <p><i>A sample lamp.config.yml file.</i></p>
       </blockquote>
 
       <p>
-        If this is your first time using a YML file, it is recommended you use our online tool to build a YML file. The YML is a very sensitive file, where one tab off or a misplaced space can prevent the program from rendering the store properly. You should always exercise caution when creating your own YML file from scratch. If you wish to see a sample of a fully functional YML file simply enter:
+        If this is your first time using a YML file, it is recommended you use our online tool to assist you in building your first one. The YML is a very sensitive file, where a tab off or a misplaced space can prevent the program from rendering the store properly. You should always exercise caution when creating your own YML file from scratch. If you wish to see a sample of a fully functional YML file simply enter:
       </p>
 
       <pre>
@@ -182,49 +196,101 @@ Models:
         Want to get started building your first YML file using our online tool? Just click here!
       </p>
 
-<hr />
-
-      <pre>
-        <code>File Structure: Rails || Ducks</code>
-      </pre>
-
-      <h3 id="rails-style">Rails Style</h3>
-
-      <blockquote>
-        <p>
-          Rails-style: separate folders for “actions”, “constants”, “reducers”,
-          “containers”, and “components”{' '}
-        </p>
-      </blockquote>
+      <h2 id="store-declaration">
+        Structure
+      </h2>
 
       <p>
-        Define the models for the store. In Rails-style, each model will each
-        receive its own sub-reducer, and the genie will assign your defined
-        types to each sub-reducer.
+        Structure determines your folder structure when you create your store. We provide users with two style options - Rails and Ducks.
       </p>
 
-      <p>Full Rails-style lamp.config.yml file: </p>
-      <div id="d-editor-one">
-        <AceEditor
-          mode="yaml"
-          theme="solarized_light"
-          id="submit-page-right"
-          value={text1}
-          height={'100%'}
-          width={'100'}
-          editorProps={{ $blockScrolling: true }}
-          readOnly={true}
-          fontSize={16}
-        />
-      </div>
       <p>
-        Place the lamp.config.yml file in the project root directory, navigate
-        there, then run:
+        Rails style separates folders for “actions”, “constants”, and “reducers”. It is most useful for smaller apps that won't have a lot of defined models.
       </p>
 
-      <pre>
-        <code>genie generate store</code>
-      </pre>
+      <p>
+        Ducks style, on the other hand, separates folders per model. This is most useful for large projects or ones that will eventually grow with scale.
+      </p>
+
+      <h2 id="store-declaration">
+        Models
+      </h2>
+
+      <p>
+        Models represent large domains of state, e.g. "users" or "countries." If the piece of state is broad enough that it requires its own reducer, then consider it a model.
+      </p>
+
+      <p>
+        When writing your model names, you should only use alphabetical characters. It should not include spaces, numbers, or special characters.
+      </p>
+
+      <p>
+        Ideally, they should be written in camelcase, e.g. 'duckDodger' or 'darkwingDuck'.
+      </p>
+
+      <h2 id="store-declaration">
+        Properties
+      </h2>
+
+      <p>
+        Properties, also referred to as your model's Slice, represent the categories of your state, e.g. "name", "address", or "phoneNumber" for a user.
+      </p>
+
+      <p>Like models, you cannot user numbers or special characters, and you should write them in camel case.</p>
+
+      <h2 id="store-declaration">
+        C.R.U.D.
+      </h2>
+
+      <p>
+        Redux Genie provides basic C.R.U.D. (Create, Read, Update, Delete) routes when it builds your models.
+      </p>
+
+      <p>
+        If you do not need these routes for a particular model, you can set CRUD to false, and that will prevent Redux Genie from creating them on store generation.
+      </p>
+
+      <h2 id="store-declaration">
+        Actions
+      </h2>
+
+      <p>
+        In the YML file, this section is defining your action creators - functions that create actions.
+      </p>
+
+      <p>
+        Actions are payloads of information that send data from your application to your store; they are the only source of information for the store.
+      </p>
+
+      <h2 id="store-declaration">
+        Thunks
+      </h2>
+
+      <p>
+        Redux Thunk middleware allows you to write action creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action, or to dispatch only if a certain condition is met.
+      </p>
+
+      <p>
+        For each model, you can define whether or not it will receive Thunks. Simply add the name of the Thunk and its endpoint, and Redux Genie will take care of the rest!
+      </p>
+
+      <p>
+        Additionally, you may also choose to have your Thunks written into a seperate file from Actions.
+      </p>
+
+      <h2 id="store-declaration">
+        Logging
+      </h2>
+
+      <p>
+        By default, Redux Genie provides basic logging middleware using Redux-logger and Redux-devtools.
+      </p>
+
+      <p>
+        If you wish, you can disable this option when creating your YML file.
+      </p>
+
+{/* <hr />
 
       <p>Output file structure:</p>
 
@@ -456,7 +522,7 @@ genie edit ducks actions
 `}
           </code>
         </div>
-      </pre>
+      </pre> */}
     </div>
   );
 };
