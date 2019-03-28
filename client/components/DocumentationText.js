2c8ee419efeda389f63055c2155c4c77f6486b36
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-03-28 11:28:55 -0400

    Fix: Nodemon running from correct directory.

--- client/components/DocumentationText.js
+++ client/components/DocumentationText.js
@@ -113,35 +113,41 @@ Models:
 
   return (
     <div id="documentation-container" className="form-style">
-
       <h1 id="reduxgenie">Redux Genie</h1>
 
       <h3 id="cheatcodesforredux">Summon the Genie!</h3>
 
       <p>
-        Redux Genie is a development tool that writes the boilerplate code for your Redux components. You can create a store from scratch, and then inject new files and their corresponding code into your project through the command line interface. Now you can spend less time managing your Redux store without abstracting its functionality, while avoiding some common errors.
+        Redux Genie is a development tool that writes the boilerplate code for
+        your Redux components. You can create a store from scratch, and then
+        inject new files and their corresponding code into your project through
+        the command line interface. Now you can spend less time managing your
+        Redux store without abstracting its functionality, while avoiding some
+        common errors.
       </p>
 
       <h2 id="store-declaration">Using this site</h2>
 
       <p>
-        By utilizing our companion website, you can easily generate a YML configuration file that contains the parameters you need to construct your Redux store. Place the YML file within your project's root directory, and then summon the genie to automatically create your front-end boilerplate.
+        By utilizing our companion website, you can easily generate a YML
+        configuration file that contains the parameters you need to construct
+        your Redux store. Place the YML file within your project's root
+        directory, and then summon the genie to automatically create your
+        front-end boilerplate.
       </p>
 
       <p>
-        Redux Genie makes starting a new project quick and simple, allowing you to get straight to writing your own code.
+        Redux Genie makes starting a new project quick and simple, allowing you
+        to get straight to writing your own code.
       </p>
 
-      <p>
-        Your wish is our command!
-      </p>
+      <p>Your wish is our command!</p>
 
-      <h2 id="store-declaration">
-        Getting Started
-      </h2>
+      <h2 id="store-declaration">Getting Started</h2>
 
       <p>
-        To begin, go to your project's root directory in your terminal and write:
+        To begin, go to your project's root directory in your terminal and
+        write:
       </p>
 
       <pre>
@@ -149,15 +155,17 @@ Models:
       </pre>
 
       <p>
-        With Redux Genie now installed, all you need to get started is a YML file.
+        With Redux Genie now installed, all you need to get started is a YML
+        file.
       </p>
 
-      <h2 id="store-declaration">
-        What's a YML file?
-      </h2>
+      <h2 id="store-declaration">What's a YML file?</h2>
 
       <p>
-        A YML file (*.yml) is a human-readable text file that allows you to define the parameters of your project's state. Redux Genie seeks out the <b>lamp.config.yml</b> file in your root directory, and like magic constructs the store according to the parameters it contains!
+        A YML file (*.yml) is a human-readable text file that allows you to
+        define the parameters of your project's state. Redux Genie seeks out the{' '}
+        <b>lamp.config.yml</b> file in your root directory, and like magic
+        constructs the store according to the parameters it contains!
       </p>
 
       <div id="d-editor-one">
@@ -175,122 +183,142 @@ Models:
       </div>
 
       <blockquote>
-        <p><i>A sample lamp.config.yml file.</i></p>
+        <p>
+          <i>A sample lamp.config.yml file.</i>
+        </p>
       </blockquote>
 
       <p>
-        If this is your first time using a YML file, it is recommended you use our online tool to assist you in building your first one. The YML is a very sensitive file, where a tab off or a misplaced space can prevent the program from rendering the store properly. You should always exercise caution when creating your own YML file from scratch. If you wish to see a sample of a fully functional YML file simply enter:
+        If this is your first time using a YML file, it is recommended you use
+        our online tool to assist you in building your first one. The YML is a
+        very sensitive file, where a tab off or a misplaced space can prevent
+        the program from rendering the store properly. You should always
+        exercise caution when creating your own YML file from scratch. If you
+        wish to see a sample of a fully functional YML file simply enter:
       </p>
 
       <pre>
-        <code>
-          $ genie sample
-        </code>
+        <code>$ genie sample</code>
       </pre>
 
       <p>
-        This will generate a sample lamp.config.yml file that you can use as a template to write your own YML.
+        This will generate a sample lamp.config.yml file that you can use as a
+        template to write your own YML.
       </p>
 
       <p>
-        Want to get started building your first YML file using our online tool? Just click here!
+        Want to get started building your first YML file using our online tool?
+        Just click here!
       </p>
 
-      <h2 id="store-declaration">
-        Structure
-      </h2>
+      <h2 id="store-declaration">Structure</h2>
 
       <p>
-        Structure determines your folder structure when you create your store. We provide users with two style options - Rails and Ducks.
-      </p>
-
-      <p>
-        Rails style separates folders for “actions”, “constants”, and “reducers”. It is most useful for smaller apps that won't have a lot of defined models.
+        Structure determines your folder structure when you create your store.
+        We provide users with two style options - Rails and Ducks.
       </p>
 
       <p>
-        Ducks style, on the other hand, separates folders per model. This is most useful for large projects or ones that will eventually grow with scale.
-      </p>
-
-      <h2 id="store-declaration">
-        Models
-      </h2>
-
-      <p>
-        Models represent large domains of state, e.g. "users" or "countries." If the piece of state is broad enough that it requires its own reducer, then consider it a model.
+        Rails style separates folders for “actions”, “constants”, and
+        “reducers”. It is most useful for smaller apps that won't have a lot of
+        defined models.
       </p>
 
       <p>
-        When writing your model names, you should only use alphabetical characters. It should not include spaces, numbers, or special characters.
+        Ducks style, on the other hand, separates folders per model. This is
+        most useful for large projects or ones that will eventually grow with
+        scale.
+      </p>
+
+      <h2 id="store-declaration">Models</h2>
+
+      <p>
+        Models represent large domains of state, e.g. "users" or "countries." If
+        the piece of state is broad enough that it requires its own reducer,
+        then consider it a model.
       </p>
 
       <p>
-        Ideally, they should be written in camelcase, e.g. 'duckDodger' or 'darkwingDuck'.
-      </p>
-
-      <h2 id="store-declaration">
-        Properties
-      </h2>
-
-      <p>
-        Properties, also referred to as your model's Slice, represent the categories of your state, e.g. "name", "address", or "phoneNumber" for a user.
-      </p>
-
-      <p>Like models, you cannot user numbers or special characters, and you should write them in camel case.</p>
-
-      <h2 id="store-declaration">
-        C.R.U.D.
-      </h2>
-
-      <p>
-        Redux Genie provides basic C.R.U.D. (Create, Read, Update, Delete) routes when it builds your models.
+        When writing your model names, you should only use alphabetical
+        characters. It should not include spaces, numbers, or special
+        characters.
       </p>
 
       <p>
-        If you do not need these routes for a particular model, you can set CRUD to false, and that will prevent Redux Genie from creating them on store generation.
+        Ideally, they should be written in camelcase, e.g. 'duckDodger' or
+        'darkwingDuck'.
       </p>
 
-      <h2 id="store-declaration">
-        Actions
-      </h2>
+      <h2 id="store-declaration">Properties</h2>
 
       <p>
-        In the YML file, this section is defining your action creators - functions that create actions.
-      </p>
-
-      <p>
-        Actions are payloads of information that send data from your application to your store; they are the only source of information for the store.
-      </p>
-
-      <h2 id="store-declaration">
-        Thunks
-      </h2>
-
-      <p>
-        Redux Thunk middleware allows you to write action creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action, or to dispatch only if a certain condition is met.
+        Properties, also referred to as your model's Slice, represent the
+        categories of your state, e.g. "name", "address", or "phoneNumber" for a
+        user.
       </p>
 
       <p>
-        For each model, you can define whether or not it will receive Thunks. Simply add the name of the Thunk and its endpoint, and Redux Genie will take care of the rest!
+        Like models, you cannot user numbers or special characters, and you
+        should write them in camel case.
+      </p>
+
+      <h2 id="store-declaration">C.R.U.D.</h2>
+
+      <p>
+        Redux Genie provides basic C.R.U.D. (Create, Read, Update, Delete)
+        routes when it builds your models.
       </p>
 
       <p>
-        Additionally, you may also choose to have your Thunks written into a seperate file from Actions.
+        If you do not need these routes for a particular model, you can set CRUD
+        to false, and that will prevent Redux Genie from creating them on store
+        generation.
       </p>
 
-      <h2 id="store-declaration">
-        Logging
-      </h2>
+      <h2 id="store-declaration">Actions</h2>
 
       <p>
-        By default, Redux Genie provides basic logging middleware using Redux-logger and Redux-devtools.
+        In the YML file, this section is defining your action creators -
+        functions that create actions.
+      </p>
+
+      <p>
+        Actions are payloads of information that send data from your application
+        to your store; they are the only source of information for the store.
+      </p>
+
+      <h2 id="store-declaration">Thunks</h2>
+
+      <p>
+        Redux Thunk middleware allows you to write action creators that return a
+        function instead of an action. The thunk can be used to delay the
+        dispatch of an action, or to dispatch only if a certain condition is
+        met.
+      </p>
+
+      <p>
+        For each model, you can define whether or not it will receive Thunks.
+        Simply add the name of the Thunk and its endpoint, and Redux Genie will
+        take care of the rest!
+      </p>
+
+      <p>
+        Additionally, you may also choose to have your Thunks written into a
+        seperate file from Actions.
+      </p>
+
+      <h2 id="store-declaration">Logging</h2>
+
+      <p>
+        By default, Redux Genie provides basic logging middleware using
+        Redux-logger and Redux-devtools.
       </p>
 
       <p>
         If you wish, you can disable this option when creating your YML file.
       </p>
 
-{/* <hr />
+      <hr />
 
       <p>Output file structure:</p>
 
@@ -522,7 +550,8 @@ genie edit ducks actions
 `}
           </code>
         </div>
-      </pre> */}
+      </pre>
+      {/* *************** */}
     </div>
   );
 };
